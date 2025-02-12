5a2a84d7083a2149c6792d0917568d3bf289a3ca
Werpyock <ttar2571@gmail.com>
2025-02-12 22:23:47 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+cd werpyockuserbot
 echo "Устанавливаем необходимые пакеты..."
 pkg update -y
 pkg upgrade -y
 pkg install -y git python openssl
-cd werpyockuserbot
 echo "Устанавливаем зависимости Python для запуска..."
 pip install --upgrade pip
 pip install telethon
