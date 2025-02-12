721742cea252832e08e7fa045625ec062ab25c37
Werpyock <ttar2571@gmail.com>
2025-02-12 22:30:24 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ pkg install -y git python openssl
 echo "Устанавливаем зависимости Python для запуска..."
 pip install --upgrade pip
 pip install telethon
+clear
 echo "Запускаю установку Werpyock Userbot.."
 python start.py
