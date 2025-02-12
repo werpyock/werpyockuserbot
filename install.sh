f9a04d1d5c3ba26f6ad3b10e86cf1275b8289b96
Werpyock <ttar2571@gmail.com>
2025-02-12 22:20:19 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ echo "Устанавливаем необходимые пакеты..."
 pkg update -y
 pkg upgrade -y
 pkg install -y git python openssl
-git clone https://github.com/werpyock/werpyockuserbot.git
 cd werpyockuserbot
 echo "Устанавливаем зависимости Python для запуска..."
 pip install --upgrade pip
